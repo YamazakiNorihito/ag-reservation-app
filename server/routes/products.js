31b2f4cacab6d759ab946aca08f968c211164323
Yamazaki Norihito <kizinn1224@gmail.com>
2021-07-13 22:46:20 +0900

    api request parameter

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,11 +1,28 @@
 // node.js express server
 const express = require('express')
-
+const Product = require('../model/product')
 
 const router = express.Router();
 
 router.get('',function(req,res){
-  res.json({'success':true});
+  Product.find({},function(err, datas)
+  {
+    res.json(datas);
+  })
 })
+
+router.get('/:productId',function(req,res){
+  const id = req.params.productId;
+  Product.findById(id,function(err, datas)
+  {
+    if(err){
+      return res.status(422).send({errors :[
+                                            {title:'Product error' ,detail:'Product not found'}
+                                  ]})
+    }
+    return res.json(datas);
+  })
+})
+
 
 module.exports = router
