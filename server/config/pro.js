51021c3509ea16fe80e2d2233513c8b79f7bb4cc
Yamazaki Norihito <kizinn1224@gmail.com>
2021-07-15 07:41:45 +0900

    本番DB切り替え設定

---
+++ server/config/pro.js
@@ -0,0 +1,3 @@
+module.exports = {
+  DB_URI : `mongodb+srv://test:testtest@cluster0.h9m65.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+}
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 // mongo db
 const mongoose = require('mongoose')
 // config
-const config = require('./config/dev')
+const config = require('./config/')   // index.js load
 // fakedb
 const FakeDb = require('./fake-db')
 // routes
@@ -18,23 +18,25 @@ mongoose.connect(config.DB_URI,
                 }
         ).then(
           () => {
-            const fakedb = new FakeDb()
-            fakedb.initDb()
+            if(ProcessingInstruction.env.MODE_ENV !== 'production'){
+              const fakedb = new FakeDb()
+              //fakedb.initDb()   // 必要な時に初期化
+            }
           }
         )
-
 ;
 
 const app = express()
 
 app.use('/api/v1/products', productRoutes)
 
-const appPath = path.join(__dirname,'..','dist','ag-reservation-app')
-app.use(express.static(appPath))
-app.get("*", function(req,res){
-  res.sendFile(path.resolve(appPath, 'index.html'))
-})
-
+if(ProcessingInstruction.env.MODE_ENV === 'production'){
+  const appPath = path.join(__dirname,'..','dist','ag-reservation-app')
+  app.use(express.static(appPath))
+  app.get("*", function(req,res){
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
 const PORT = process.env.PORT || '3001'
 app.listen(PORT,function(){
   console.log('I am running')
