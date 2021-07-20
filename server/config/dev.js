0f19cf17096dc9d2a01057e0565d0bb9ae97f853
Yamazaki Norihito <kizinn1224@gmail.com>
2021-07-20 20:42:22 +0900

    mongodb insert user

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  DB_URI : 'mongodb+srv://testuser:testtest@cluster0.qumby.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
+  DB_URI:
+    "mongodb+srv://testuser:testtest@cluster0.qumby.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -14,8 +14,61 @@ router.post("/login", function (req, res) {});
 router.post("/register", function (req, res) {
   let name = req.body.username;
   let pw = req.body.password;
+  let pw_conf = req.body.confirmPassword;
   let email = req.body.email;
-  return res.json({ name, email, pw });
+
+  if (!name) {
+    return res.status(422).send({
+      errors: [{ title: "username error", detail: "Please fill username" }],
+    });
+  }
+
+  if (!pw) {
+    return res.status(422).send({
+      errors: [{ title: "password error", detail: "Please fill password" }],
+    });
+  }
+
+  if (pw !== pw_conf) {
+    return res.status(422).send({
+      errors: [
+        { title: "password error", detail: "Please check unmachi password" },
+      ],
+    });
+  }
+
+  if (!email) {
+    return res.status(422).send({
+      errors: [{ title: "email error", detail: "Please fill email" }],
+    });
+  }
+
+  User.findOne({ email }, function (err, foundUser) {
+    if (err) {
+      return res.status(422).send({
+        errors: [{ title: "User error", detail: "Somathing went wrong!!" }],
+      });
+    }
+
+    if (foundUser) {
+      return res.status(422).send({
+        errors: [{ title: "User error", detail: "User already exists!!" }],
+      });
+    }
+
+    let password = pw;
+    const user = new User({ name, email, password });
+
+    user.save(function (err) {
+      if (err) {
+        return res.status(422).send({
+          errors: [{ title: "User error", detail: "Somathing went wrong!!" }],
+        });
+      }
+
+      return res.json({ register: true });
+    });
+  });
 });
 
 module.exports = router;
