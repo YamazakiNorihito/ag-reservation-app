b69c0e8957876e1a22e0e0a9a9ebbf717d6c91a7
Yamazaki Norihito <kizinn1224@gmail.com>
2021-07-21 22:45:56 +0900

    微修正

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,4 @@
 module.exports = {
-  DB_URI:
-    "mongodb+srv://testuser:testtest@cluster0.qumby.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  DB_URI: "mongodb+srv://<id>:<pw>@cluster0.qumby.mongodb.net/myFirstDatabase?",
   SECRET: "A%D*G-KaPdSgVkYp3s6v8y/B?E(H+MbQ",
 };
