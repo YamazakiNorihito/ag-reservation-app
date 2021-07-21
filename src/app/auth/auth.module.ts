9aa62af05236e21360edcc83bd6797a785535783
Yamazaki Norihito <kizinn1224@gmail.com>
2021-07-21 16:25:44 +0900

    client auth guard

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
-import {LoginComponent} from './login/login.component'
-import {RegisterComponent} from './register/register.component'
+import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './register/register.component'
 
-import {AuthService} from './shared/auth.service'
+import { AuthService } from './shared/auth.service'
 import { FormsModule } from '@angular/forms';
+import { AuthGuard } from './shared/auth.guard';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FormsModule } from '@angular/forms';
     AuthRoutingModule,
     FormsModule
   ],
-  providers :[
+  providers: [
+    AuthGuard,
     AuthService
   ]
 })
--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListingsComponent } from './product-listings/product-listings.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { AuthGuard } from '../auth/shared/auth.guard';
 
 const routes: Routes = [
-  {path : '', component: ProductListingsComponent},
-  {path : ':productId', component: ProductDetailComponent}
+  { path: '', component: ProductListingsComponent },
+  { path: ':productId', component: ProductDetailComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
